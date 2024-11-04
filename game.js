ef996858f46d793826a3dafffae53cce889245c6
Joe19071991 <lauranove66@gmail.com>
2024-11-04 12:40:22 +0100

    forEach option + création du bouton + ajout au conteneur

--- game.js
+++ game.js
@@ -2,12 +2,16 @@ import { quiz_fatoumata_kebe } from "./question.js";
 const question = document.querySelector(".question");
 const options = document.querySelector(".options");
 console.log("question", quiz_fatoumata_kebe);
-console.log(
-  "1erequestion",
-  quiz_fatoumata_kebe.questions[0]
-); /*appel de la première question question text bonne réponse*/
-const currentQuestion =
-  quiz_fatoumata_kebe
-    .questions[3]; /*appel de la première question question text bonne réponse*/
-question.innerText =
-  currentQuestion.text; /* récupère l'emplacement de question pui snous avons changé le texte de la question */
+console.log("1erequestion", quiz_fatoumata_kebe.questions[2]);
+/*appel de la première question question text bonne réponse*/
+const currentQuestion = quiz_fatoumata_kebe.questions[1];
+/*appel de la première question question text bonne réponse*/
+question.innerText = currentQuestion.text;
+/* récupère l'emplacement de question pui snous avons changé le texte de la question */
+
+currentQuestion.options.forEach((optionText) => {
+  const optionButton = document.createElement("button");
+  optionButton.innerText = optionText;
+  optionButton.classList.add(optionText);
+  options.appendChild(optionButton);
+});
