1b574844439a7dadc707fab2bd4dd8377e02b7d3
Léandre Lavenu <leandre.lavenu@outlook.com>
2024-11-04 15:24:13 +0100

    nettoyage html + modification game.js : classList

--- game.js
+++ game.js
@@ -1,17 +1,24 @@
 import { quiz_fatoumata_kebe } from "./question.js";
+
 const question = document.querySelector(".question");
 const options = document.querySelector(".options");
+
 console.log("question", quiz_fatoumata_kebe);
-console.log("1erequestion", quiz_fatoumata_kebe.questions[2]);
+console.log("1erequestion", quiz_fatoumata_kebe.questions[1]);
 /*appel de la première question question text bonne réponse*/
-const currentQuestion = quiz_fatoumata_kebe.questions[1];
+
+const currentQuestion = quiz_fatoumata_kebe.questions[0];
 /*appel de la première question question text bonne réponse*/
+
 question.innerText = currentQuestion.text;
-/* récupère l'emplacement de question pui snous avons changé le texte de la question */
+/* récupère l'emplacement de question puis nous avons changé le texte de la question */
 
 currentQuestion.options.forEach((optionText) => {
   const optionButton = document.createElement("button");
   optionButton.innerText = optionText;
-  optionButton.classList.add(optionText);
+
+  // optionButton.classList.add(optionText);
+  // BLOQUE CERTAINES QUESTIONS ET REPONSES
+
   options.appendChild(optionButton);
 });
