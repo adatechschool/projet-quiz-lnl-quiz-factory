5ba0ce48a57f0bcef459592321a8981b9764d264
Joe19071991 <lauranove66@gmail.com>
2024-11-05 15:09:16 +0100

    re-push

--- game.js
+++ game.js
@@ -12,17 +12,15 @@ function loadQuestion() {
   optionsElement.innerText = "";
   const currentQuestion = quiz_fatoumata_kebe.questions[currentQuestionIndex];
   questionElement.innerText = currentQuestion.text;
-
   currentQuestion.options.forEach((optionsText) => {
     const optionButton = document.createElement("button");
     optionButton.innerText = optionsText;
-    optionsButton.appendChild(optionButton);
+    optionsElement.appendChild(optionButton);
   });
 }
 
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
-
 });
 
 if (currentQuestionIndex < quiz_fatoumata_kebe.questions.length) {
@@ -32,30 +30,5 @@ if (currentQuestionIndex < quiz_fatoumata_kebe.questions.length) {
   optionsElement.innerHTML = "";
   nextButton.style.display = "none";
 }
-
-
-  console.log(
-    "currentQuestionIndex est maintenant égal a " + currentQuestionIndex
-  );
-
-  console.log(
-    "Est-ce que il reste des questions ? (" +
-      currentQuestionIndex +
-      " < " +
-      quiz_fatoumata_kebe.questions.length +
-      " = " +
-      (currentQuestionIndex < quiz_fatoumata_kebe.questions.length) +
-      ")"
-  );
-  if (currentQuestionIndex < quiz_fatoumata_kebe.questions.length) {
-    loadQuestion();
-  } else {
-    console.log("On indique la fin du quiz !");
-    questionElement.innerText = "Fin !";
-    optionsElement.innerHTML = "";
-    nextButton.style.display = "none";
-  }
-});
-
 
 loadQuestion();
